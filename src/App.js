67a5c248d3d9ab7f79ce7403fe33c0334180a24d
Sergio Rolando Diaz Ventura <sergi0r0ladiaz15@gmail.com>
2022-05-13 11:19:19 -0500

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [cuenta, setCuenta]= useState(0);
+  const [paso, setPaso]= useState(1);
+
+ const handleClick = ()=> {
+    console.log(paso);
+    setCUenta(cuenta-paso);
+  };
+  const handleClick1 = ()=> {
+    setCuenta(0);
+    setPaso(1);
+  };
+
+  const handleClick0 = ()=> {
+    setCuenta(cuenta +paso);
+  };
+  const handleInputCHange =(e)=>{
+    if(isNaN(e.target.value)){
+      return;
+    }
+    setPaso(Number(e.terget.value));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+    <h3>Contador</h3> 
+    <hr />
+    <h2 className="text-center">{cuenta}</h2>
+    <hr />
+    <div 
+    style={{
+      display:"flex",
+      justifyContent:"flex-end",
+      marginRight:"5px",
+    }}
+      >
+        <div class="input-group me-3">
+    
     </div>
   );
 }
